e524abc6260814d83f50d7417aed1e57c96be3f3
RhedOrca <33771009+RhedOrca@users.noreply.github.com>
2019-01-21 12:44:54 -0800

    Update Eyes.py
    commented for clarity.

--- Eyes.py
+++ Eyes.py
@@ -10,16 +10,16 @@ targets = ['https://www.bloomberg.com/feeds/bbiz/sitemap_recent.xml', 'https://t
 for target in targets:
     page = urllib2.urlopen(target)
     soup = BeautifulSoup(page, 'html.parser') #converts target page into soup, presumably alphabet.
-    texts = soup.findAll(text=True) #creates a comma separated list elements in soup that are text
+    texts = soup.findAll(text=True) #creates a comma separated list of the elements in soup that are text
     print texts
 
     with open('news_headlines.csv', 'ar') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         for selection in texts: #filters the urls from the CSL
-            if 'articles' in selection:
+            if 'articles' in selection: #bloomberg
                 print selection
-                filewriter.writerow([selection[40:50], selection[51:], now])
-            elif '<b>' in selection:
-                filewriter.writerow(['GGL'+str(now), selection.encode("utf-8"), now])
+                filewriter.writerow([selection[40:50], selection[51:], now]) #published date, headline, recording date
+            elif '<b>' in selection: #google trending headline
+                filewriter.writerow(['GGL'+str(now), selection.encode("utf-8"), now]) #effective "publishing" date with GGL prefix, headline, recording date
                 print selection
-                print "---------THIS IS A LINE BREAK BETWEEN SELECTIONS------------"
+                print "---THIS IS A LINE BREAK---"
